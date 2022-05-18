7416737e0d5bbb4b4e4d608b8169ea06881dfa01
Joe Richardson <Joe.Richardson@sicon.co.uk>
2022-05-18 14:07:24 +0100

    Added controller factory

--- RecipeMateTests/RecipeTests/RecipeApiTests.cs
+++ RecipeMateTests/RecipeTests/RecipeApiTests.cs
@@ -1,9 +1,16 @@
 ﻿using Moq;
 using RecipeMateDomain.Service;
 using RecipeMateModels.Models.Recipe;
+using RecipeMateModels.Models.Step;
 using RecipeMateWebAPI.EndPoints.Recipes;
 using RecipeMateWebAPI.Validator;
 using System.Threading.Tasks;
+using RecipeMateModels.RequestModels;
+using RecipeMateModels.RequestModels.Ingredient;
+using RecipeMateModels.RequestModels.Equipment;
+using RecipeMateModels.Models.Units;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace RecipeMateTests.RecipeTests
 {
@@ -13,15 +20,43 @@ namespace RecipeMateTests.RecipeTests
         [Test]
         public async Task CreateSuccesfulAsync()
         {
-            var recipe = new RecipeModel{};
-
+            var recipe = new RecipeModel
+            {
+                UserId = "Bzzt",
+                Steps = new()
+                {
+                    new StepModel
+                    {
+                        Step = "Chop Onion",
+                        Equipment = new()
+                        {
+                            new EquipmentModel
+                            {
+                                Name = "Knife"
+                            }
+                        },
+                        Ingredients = new()
+                        {
+                            new IngredientModel
+                            {
+                                Name = "Onion" 
+                            }
+                        }
+                    }
+                },
+                Ingredients = new()
+                {
+                    new IngredientModel
+                    {
+                        Name = "Onion"
+                    }
+                }
+            };
             var recipeService = new Mock<IRecipeService>();
-   
 
             recipeService.Setup(x => x.AddRecipeAsync(It.IsAny<RecipeModel>())).ReturnsAsync(recipe);
 
-            var result = await RecipeEndPoints.CreateAsync(recipeService.Object, new RecipeValidator(), recipe);
-
+            Task<IResult> result = await RecipeEndPoints.CreateAsync(recipeService.Object, new RecipeValidator(), recipe);
         }
     }
 }
